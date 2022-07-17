562908d7c35f86060d6468bcf162a848e80559ba
vishal <vishalshinde00126@gmail.com>
2022-07-17 08:29:48 +0530

    first commit

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ updatRecord(formData);
 function readFormData()
 {
     var formData={};
-    formData["srNo"]= document.getElementById("srNo").value ;
+    formData["EmpNo"]= document.getElementById("EmpNo").value ;
     formData["firstName"]= document.getElementById("firstName").value;
     formData["lastName"]= document.getElementById("lastName").value;
+    formData["Password"]= document.getElementById("Password").value;
+    formData["pwd"]= document.getElementById("pwd").value;
     return formData;
 
 }
@@ -30,27 +32,36 @@ function insertNewRecord(data){
     var table = document.getElementById("storelist").getElementsByTagName('tbody')[0];
     var newRow= table.insertRow(table.length);
     var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.srNo;
+        cell1.innerHTML = data.EmpNo;
     var cell2 = newRow.insertCell(1);
         cell2.innerHTML = data.firstName;
     var cell3 = newRow.insertCell(2);
         cell3.innerHTML = data.lastName;
-     var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = `<button onclick='onEdit(this)' > Edit </button> <button onclick='onDelete(this)'> Delete</button>`
+    var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.Password;
+     var cell5 = newRow.insertCell(4);
+        cell5.innerHTML = data.pwd;
+    var cell6 = newRow.insertCell(5);
+        cell6.innerHTML = `<button onclick='onEdit(this)' > Edit </button> <button onclick='onDelete(this)'> Delete</button>`
+        
 }
 
 function  onEdit(td)
 {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById('srNo').value = selectedRow.cells[0].innerHTML
+    document.getElementById('EmpNo').value = selectedRow.cells[0].innerHTML
     document.getElementById('firstName').value = selectedRow.cells[1].innerHTML
     document.getElementById('lastName').value = selectedRow.cells[2].innerHTML
+    document.getElementById('Password').value = selectedRow.cells[3].innerHTML
+    document.getElementById('pwd').value = selectedRow.cells[4].innerHTML
     
 }
 function updatRecord(formData)
-{selectedRow.cells[0].innerHTML = formData.srNo;
+{selectedRow.cells[0].innerHTML = formData.EmpNo;
     selectedRow.cells[1].innerHTML = formData.firstName;
     selectedRow.cells[2].innerHTML = formData.lastName;
+    selectedRow.cells[3].innerHTML = formData.Password;
+    selectedRow.cells[4].innerHTML = formData.pwd;
 
 }
 
@@ -68,9 +79,12 @@ function onDelete(td){
 
 function resetForm()
 {
-    document.getElementById('srNo').value= '';
+    document.getElementById('EmpNo').value= '';
     document.getElementById('firstName').value= '';
     document.getElementById('lastName').value= '';
+    document.getElementById('Password').value= '';
+    document.getElementById('pwd').value= '';
+
 
 }
 
